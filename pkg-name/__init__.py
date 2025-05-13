e87653a34df721369c84318e24bfd64fb2e71e14
E33605 <debmalyapramanik.005@gmail.com>
2025-05-13 16:37:08 +0530

    💣🩹 set the default python requirement as 3.10, fix version name

--- pkg-name/__init__.py
+++ pkg-name/__init__.py
@@ -11,9 +11,11 @@ Docker for added capabilities.
 @copywright: 2021; Debmalya Pramanik
 """
 
+import os
+
 # ? package follows https://peps.python.org/pep-0440/
 # ? https://python-semver.readthedocs.io/en/latest/advanced/convert-pypi-to-semver.html
-__version__ = "v0.0.1.dev0"
+__version__ = open(os.path.join(os.path.dirname(__file__), 'VERSION'), 'r').read().strip()
 
 # ! let's check for package hard dependencies which must be available
 hard_dependencies = [] # all should be available in ../requirements.txt
@@ -30,4 +32,4 @@ if missing_dependencies:
     raise ImportError(f"Missing hard dependencies: {", ".join(missing_dependencies)}")
 
 # init-time Option Registrations
-from .api import *
+from .api import * # noqa: F401, F403 # pyright: ignore[reportMissingImports]
