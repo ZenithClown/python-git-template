0a2c26365a9562b124de598299a739369df2b1f9
Debmalya Pramanik <debmalyapramanik.005@gmail.com>
2025-05-13 16:21:45 +0530

    🐛 Update __init__.py
    - use default snake_case formatting
    - use spaces (4) for indentation
    - add template module docs

--- pkg-name/__init__.py
+++ pkg-name/__init__.py
@@ -1,23 +1,33 @@
 # -*- encoding: utf-8 -*-
 
-__author__    = 'Debmalya Pramanik'
-__copyright__ = f"Copyright (c) [year] {__author__}"
+"""
+Python GIT & Docker Template for PyPI Package
 
-# __status__    = ""
-# __docformat__ = ""
+The package provides a template repository to create a python package,
+that can be seamlessly deployed on PyPI and configured via GIT and
+Docker for added capabilities.
 
-# Let's Check for the Dependencies
-hardDependencies    = ['numpy'] # remove/update from setup.py
-missingDependencies = []
+@author: Debmalya Pramanik
+@copywright: 2021; Debmalya Pramanik
+"""
 
-for dependency in hardDependencies:
-	try:
-		__import__(dependency)
-	except ImportError:
-		missingDependencies.append(dependency)
+# ? package follows https://peps.python.org/pep-0440/
+# ? https://python-semver.readthedocs.io/en/latest/advanced/convert-pypi-to-semver.html
+__version__ = "v0.0.1.dev0"
 
-if missingDependencies:
-	raise ImportError('Required Dependencies {}'.format(missingDependencies))
+# ! let's check for package hard dependencies which must be available
+hard_dependencies = [] # all should be available in ../requirements.txt
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(e.name)
+
+# ! raise an error during import if any hard package is missing
+if missing_dependencies:
+    raise ImportError(f"Missing hard dependencies: {", ".join(missing_dependencies)}")
 
 # init-time Option Registrations
 from .api import *
